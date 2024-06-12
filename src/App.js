93b979fe542e2b00a5919956bcc0c883ed4d929e
BrunoTrndd <40573273+BrunoTrndd@users.noreply.github.com>
2024-06-12 00:17:13 -0300

    Add image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import UploadBox from './components/uploadBox';
 import UploadButton from './components/uploadButton';
 import './style/App.css';
 
+
+// Cria uma instância do mock adapter
+const mock = new MockAdapter(axios, { delayResponse: 500 });
+
+const randomImageUrl = 'https://picsum.photos/600/400'; // URL para obter uma imagem aleatória
+
 function App() {
   const [file, setFile] = useState(null);
-  const [chartData, setChartData] = useState(null);
+  const [imageSrc, setImageSrc] = useState(null);
 
   const handleDrop = (acceptedFiles) => {
     setFile(acceptedFiles[0]);
   };
+
+  useEffect(() => {
+    // Configura o mock para a rota de upload
+    mock.onPost('http://localhost:5000/upload').reply(200, {
+      imageUrl: randomImageUrl,
+    });
+  }, []);
 
   const handleUpload = async () => {
     if (!file) {
@@ -22,15 +36,18 @@ function App() {
     formData.append('file', file);
 
     try {
-      const response = await axios.post('YOUR_ENDPOINT_URL', formData, {
+      const response = await axios.post('http://localhost:5000/upload', file, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      setChartData(response.data);
+
+      // Usa a URL da imagem mockada
+      setImageSrc(response.data.imageUrl);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
+
   };
 
   const handleClassification = () => {
@@ -47,13 +64,18 @@ function App() {
     <div className="App">
       <h1>Upload File and Display Chart</h1>
       <div className="content">
-        <UploadBox onDrop={handleDrop} />
+        <UploadBox onDrop={handleDrop} fileName={file ? file.name : null} />
         <div className="buttons">
           <UploadButton text="Classificação" onClick={handleClassification} />
           <UploadButton text="Agrupamento" onClick={handleGrouping} />
           <UploadButton text="Upload" onClick={handleUpload} />
         </div>
       </div>
+      {imageSrc && (
+        <div className="image-container">
+          <img src={imageSrc} alt="Processed result" />
+        </div>
+      )}
     </div>
   );
 }
